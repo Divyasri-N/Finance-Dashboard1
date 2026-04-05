8c2e8754f688c5fdff8240a66640c0eb1f9ae411
DIVYASRI N <dhivyadhivyasri1234@gmail.com>
2026-04-05 14:51:23 +0530

    fixed useStore import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Dashboard from "./pages/Dashboard";
 import Insights from "./pages/Insights";
-import { useStore } from "./store/useStore";
+import { useStore } from "./store/useStore.js";
 import { transactions } from "./data/mockData";
 
 function App() {
